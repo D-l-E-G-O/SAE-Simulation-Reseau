b4e08cb42942f7d1b38e42999aa0ecba3cb3587e
Diego Aparicio <diego.aparicio@etu.unistra.fr>
2025-05-20 17:32:33 +0200

    fonctions IPV4 adress.c

--- code/adress.c
+++ code/adress.c
@@ -1,31 +1,44 @@
 #pragma one
-#include "adresse.h"
+#include "adress.h"
 
 
+bool compare_mac(mac const* mac1, mac const* mac2){
+    bool equal = true;
+    size_t i = 0;
+    while(i<6 && equal){
+        equal = (mac1[i]&mac2[i] == mac1[i]);
+    }
+    return equal;
+};
 
-void init_mac(){
+char* to_string_mac(mac const* mac, char * buffer){
 
 };
-void init_ipv4(){
 
-};
+void afficher_mac(mac const* mac);
 
-bool compare_mac(mac const * mac1, mac const* mac2){
 
-};
-char* to_string_mac(mac const * mac,char * buffer){
 
-};
 
 bool compare_ipv4( ipv4 const* ip1, ipv4 const* ip2){
-
-};
-char* to_string_ipv4(ipv4 const * ip,char * buffer){
-    
+    return ip1&ip2 == ip1;
 };
 
+char* to_string_ipv4(ipv4 const* ip, char* buffer){
+    for(size_t i=1; i<=4; i++){
+        u_int32_t masque = (1 << (i*8)) - 1;
+        uint8_t x = (ip&masque) >> (32 - i*8);
+        if (i<4){
+            sprintf(buffer, "%zu.", x);
+        }
+        else{
+            sprintf(buffer, "%zu", x);
+        }
+    }
+};
 
-void afficher_mac( mac const* mac);
-void afficher_ip(ipv4 const  *ip);
+void afficher_ip(ipv4 const *ip){
+    printf("IPV4: %s", to_string_ipv4(ip));
+};
 
 
--- code/adress.h
+++ code/adress.h
@@ -1,21 +1,17 @@
 #pragma once
 #include <stdint.h> 
 #include <stdbool.h>
+
+
 typedef uint8_t mac[6];
-
-typedef uint32_t ipv4;
-
-void init_mac();
-void init_ipv4();
-
 bool compare_mac(mac const * mac1, mac const* mac2);
 char* to_string_mac(mac const * mac,char * buffer);
+void afficher_mac( mac const* mac);
 
+
+typedef uint32_t ipv4;
 bool compare_ipv4( ipv4 const* ip1, ipv4 const* ip2);
 char* to_string_ipv4(ipv4 const * ip,char * buffer);
-
-
-void afficher_mac( mac const* mac);
 void afficher_ip(ipv4 const  *ip);
 
 
